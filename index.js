1926eca9a3fcafd0fc63a52cf2b7a80faecd4b25
NickBird <almabroral@gmail.com>
2022-10-02 13:10:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3009
 
 const { connectDatabase } = require("./src/utils/db");
 connectDatabase().then(() => {console.log("Connected to DB. Initializing client."); });
