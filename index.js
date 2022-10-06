6c6df7c3e40137b7d38773446a5fbe021987500e
Nik Bird <40806293+NikBird@users.noreply.github.com>
2022-10-06 14:16:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3009
 
 const { connectDatabase } = require("./src/utils/db");
 connectDatabase().then(() => {console.log("Connected to DB. Initializing client."); });
-
+require('./src/utils/db');
 const server = app.listen(port, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
